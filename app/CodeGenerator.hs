b28ec2c1c6a5b99900b4a5951dcb16750f86d881
Tim Palade <timotei@cliqz.com>
2017-05-18 11:48:01 +0200

    Fixes to Elm Code Generation

--- app/CodeGenerator.hs
+++ app/CodeGenerator.hs
@@ -23,9 +23,9 @@ import Servant.Elm
     )
 
 import Api           (ChannelApi)
-import Types.Message (Message)
+import Types.Message (Message, Attachment, Reaction)
 import Types.Channel (Channel)
-import Types.User    (User)
+import Types.User    (User, Profile)
 
 
 elmOpts :: ElmOptions
@@ -37,12 +37,21 @@ spec :: Spec
 spec =
   Spec ["Generated", "Api"]
     (defElmImports
+    : toElmTypeSource    (Proxy :: Proxy Attachment)
+    : toElmEncoderSource (Proxy :: Proxy Attachment)
+    : toElmDecoderSource (Proxy :: Proxy Attachment)
+    : toElmTypeSource    (Proxy :: Proxy Reaction)
+    : toElmEncoderSource (Proxy :: Proxy Reaction)
+    : toElmDecoderSource (Proxy :: Proxy Reaction)
     : toElmTypeSource    (Proxy :: Proxy Message)
     : toElmDecoderSource (Proxy :: Proxy Message)
     : toElmEncoderSource (Proxy :: Proxy Message)
     : toElmTypeSource    (Proxy :: Proxy Channel)
     : toElmDecoderSource (Proxy :: Proxy Channel)
     : toElmEncoderSource (Proxy :: Proxy Channel)
+    : toElmTypeSource    (Proxy :: Proxy Profile)
+    : toElmDecoderSource (Proxy :: Proxy Profile)
+    : toElmEncoderSource (Proxy :: Proxy Profile)
     : toElmTypeSource    (Proxy :: Proxy User)
     : toElmDecoderSource (Proxy :: Proxy User)
     : toElmEncoderSource (Proxy :: Proxy User)
