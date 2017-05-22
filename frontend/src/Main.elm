0ea2f6af339f13cb980b6dc608b0d5e5e05993da
Tim Palade <timotei@cliqz.com>
2017-05-22 10:46:08 +0200

    Seoarated UI into components

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -31,6 +31,7 @@ init : (Model, Cmd Msg)
 init =
     ( Model [] Nothing [] Nothing
     , Http.send NewChannels getChannel
+    --, Http.send NewMessages (getChannelByName "officetalk")
     )
     -- TODO: emit a command to fetch the list of messages
 
@@ -38,6 +39,7 @@ init =
 
 type Msg =
       NewChannels       (Result Http.Error (List Channel))
+    | Channel           (Channel)
     | NewMessages       (Result Http.Error (List Message))
     | FilterMessages    (String)
 
@@ -45,8 +47,13 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         NewChannels (Ok channels) ->
+            -- *Debug.log(List.foldl (++) "" (List.map (\t -> " " ++ t.name) channels))
             ( { model | channels = channels }
-            , Http.send NewMessages (getChannelByName "officetalk")
+            , Cmd.none
+            )
+        Channel channel ->
+            ( { model | currentChannel = Just channel.name }
+            , Http.send NewMessages (getChannelByName channel.name)
             )
         NewMessages (Ok messages) ->
             ( { model | currentMessages = messages }
@@ -80,23 +87,124 @@ filterMessages pattern strings =
 
 
 view : Model -> Html Msg
-view ({ currentMessages, pattern }) =
+view ({ channels ,currentMessages, pattern }) =
     let
         strings = getMessages currentMessages
         messages = filterMessages pattern strings
+        --channel_names = List.map (\t -> t.name) channels
     in
-        div []
-          [ input
-          [ placeholder "filter"
-          , autofocus True
-          , onInput FilterMessages
-          ] []
-          , ol []
-          (List.map (\t -> text t) messages)
-        ]
+        div [] -- * container with channel div and messages div
+          [ channelComponent channels
+          , messagesComponent messages
+          ]
 
 
 -- * SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.none
+
+-- * VISUAL COMPONENTS
+
+channelComponent channels =
+    div [inlineStyle, sclollableContainerStyle, channelsStyle, channelPrimaryBgColor]
+      [ channelHeader
+      , channelNames channels
+      ]
+
+channelHeader =
+    div []
+      [ div [whiteFontColor] [text "Cliqz"]
+      , div []
+        [ div [circleStyle, inlineStyle] []
+        , div [inlineStyle, channelPrimaryFontColor] [text "Slack History"]
+        ]
+      ]
+
+channelNames channels =
+    div [] (List.map (\t -> channelName t) channels)
+
+channelName channel =
+    div [sansSerifFont, channelNamesStyle, channelPrimaryFontColor, onClick (Channel channel)]
+      [ text ("#" ++ channel.name)
+      ]
+
+messagesComponent messages =
+    div [inlineStyle, sclollableContainerStyle, messageContainerStyle]
+    [ --searchBarComponent
+    --,
+      div [] (List.map (\t -> messageCell t) messages)
+    ]
+
+searchBarComponent =
+    input [placeholder "filter", autofocus True, onInput FilterMessages] []
+
+messageCell message =
+    div [sansSerifFont, messagesStyle]
+      [ text message
+      ]
+
+-- * STYLES
+
+sclollableContainerStyle =
+  style
+    [ ("left", "0")
+    , ("right", "0")
+    , ("overflow-y", "scroll")
+    , ("height", "100vh")
+    ]
+
+channelsStyle =
+  style
+    [ ("width", "20%")
+    , ("float", "left")
+    ]
+
+channelNamesStyle =
+  style
+    [ ("font-size", "16px")
+    , ("text-align", "left")
+    ]
+
+channelPrimaryBgColor =
+  style
+    [("background-color", "#4D394B")]
+
+channelPrimaryFontColor =
+  style
+    [("color", "#AB9BA9")]
+
+whiteFontColor =
+  style
+   [("color", "#FFFFFF")]
+
+messageContainerStyle =
+  style
+    [ ("width", "80%")
+    , ("float", "right")
+    , ("background-color", "#FFFFFF")
+    ]
+
+messagesStyle =
+  style
+    [ ("font-size", "14px")
+    , ("text-align", "left")
+    ]
+
+circleStyle =
+  style
+    [ ("background-color", "#38978D")
+    , ("width", "10px")
+    , ("height", "10px")
+    , ("border-radius", "50%")
+    ]
+
+inlineStyle =
+  style
+    [ ("display", "inline-block")
+    ]
+
+sansSerifFont =
+  style
+    [ ("font-family", "Sans-Serif")
+    ]
